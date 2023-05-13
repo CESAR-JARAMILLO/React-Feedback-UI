22400ca257cc41bb7609db4c53bea375c7ab3d61
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-05-12 18:00:02 -0700

    added FeedbackList and added data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import FeedbackData from './data/FeedbackData';
+import { useState } from 'react'
 import Header from './components/Header'
-import FeedbackItem from './components/FeedbackItem'
+import FeedbackList from './components/FeedbackList'
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
+
   return (
     <>
       <Header text='Feedback UI' />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   )

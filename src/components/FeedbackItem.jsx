cb6ecaae21e34a2059b0b0fb679e74312a2d14e6
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-05-13 21:00:57 -0700

    created delete button and passed prop

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,23 +1,23 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
-function FeedbackItem({ text, rating }) {
-
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">
-        {rating}
+        {item.rating}
       </div>
+      <button onClick={() => handleDelete(item.id)} className='close'><FaTimes color='purple' /></button>
       <div className="text-display">
-        {text}
+        {item.text}
       </div>
     </Card>
   )
 }
 
 FeedbackItem.propTypes = {
-  text: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
+  item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem
